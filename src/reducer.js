a624dafaa30ce92a30d601dc4493fc445df0bc1b
earthlingJen <jk.winkler@rocketmail.com>
2018-09-10 18:08:43 +0200

    Change if elses into switch-cases

--- src/reducer.js
+++ src/reducer.js
@@ -1,62 +1,128 @@
 export default function reducer(state, action = {}) {
-  if (action.type === 'DELETE_ALL_PLAYERS') {
-    return {
-      ...state,
-      players: [],
-    }
-  } else if (action.type === 'ADD_PLAYER') {
-    return {
-      ...state,
-      players: [
-        ...state.players,
-        {
-          name: action.payload.name,
-          scores: [],
-          roundScore: 0,
-        },
-      ],
-    }
-  } else if (action.type === 'RESET_SCORE') {
-    return {
-      players: state.players.map(player => ({
-        ...player,
-        scores: [],
-      })),
-    }
-  } else if (action.type === 'DELETE_PLAYER') {
-    return {
-      ...state,
-      players: [
-        ...state.players.slice(0, action.payload.index),
-        ...state.players.slice(action.payload.index + 1),
-      ],
-    }
-  } else if (action.type === 'SAVE_ROUND') {
-    return {
-      ...state,
-
-      players: state.players.map(player => {
-        return {
+  switch (action.type) {
+    case 'DELETE_ALL_PLAYERS':
+      return {
+        ...state,
+        players: [],
+      }
+    case 'ADD_PLAYER':
+      return {
+        ...state,
+        players: [
+          ...state.players,
+          {
+            name: action.payload.name,
+            scores: [],
+            roundScore: 0,
+          },
+        ],
+      }
+    case 'RESET_SCORE':
+      return {
+        players: state.players.map(player => ({
           ...player,
-          scores: [...player.scores, player.roundScore],
-          roundScore: 0,
-        }
-      }),
-    }
-  } else if (action.type === 'UPDATE_ROUNDSCORE') {
-    return {
-      ...state,
-      players: [
-        ...state.players.slice(0, action.payload.index),
-        {
-          ...state.players[action.payload.index],
-          roundScore:
-            state.players[action.payload.index].roundScore +
-            action.payload.value,
-        },
-        ...state.players.slice(action.payload.index + 1),
-      ],
-    }
+          scores: [],
+        })),
+      }
+    case 'DELETE_PLAYER':
+      return {
+        ...state,
+        players: [
+          ...state.players.slice(0, action.payload.index),
+          ...state.players.slice(action.payload.index + 1),
+        ],
+      }
+    case 'SAVE_ROUND':
+      return {
+        ...state,
+
+        players: state.players.map(player => {
+          return {
+            ...player,
+            scores: [...player.scores, player.roundScore],
+            roundScore: 0,
+          }
+        }),
+      }
+    case 'UPDATE_ROUNDSCORE':
+      return {
+        ...state,
+        players: [
+          ...state.players.slice(0, action.payload.index),
+          {
+            ...state.players[action.payload.index],
+            roundScore:
+              state.players[action.payload.index].roundScore +
+              action.payload.value,
+          },
+          ...state.players.slice(action.payload.index + 1),
+        ],
+      }
   }
+
+  // if (action.type === 'DELETE_ALL_PLAYERS') {
+  //   return {
+  //     ...state,
+  //     players: [],
+  //   }
+
+  // } else if (action.type === 'ADD_PLAYER') {
+  //   return {
+  //     ...state,
+  //     players: [
+  //       ...state.players,
+  //       {
+  //         name: action.payload.name,
+  //         scores: [],
+  //         roundScore: 0,
+  //       },
+  //     ],
+  //   }
+
+  // } else if (action.type === 'RESET_SCORE') {
+  //   return {
+  //     players: state.players.map(player => ({
+  //       ...player,
+  //       scores: [],
+  //     })),
+  //   }
+
+  // } else if (action.type === 'DELETE_PLAYER') {
+  //   return {
+  //     ...state,
+  //     players: [
+  //       ...state.players.slice(0, action.payload.index),
+  //       ...state.players.slice(action.payload.index + 1),
+  //     ],
+  //   }
+
+  // } else if (action.type === 'SAVE_ROUND') {
+  //   return {
+  //     ...state,
+
+  //     players: state.players.map(player => {
+  //       return {
+  //         ...player,
+  //         scores: [...player.scores, player.roundScore],
+  //         roundScore: 0,
+  //       }
+  //     }),
+  //   }
+
+  // } else if (action.type === 'UPDATE_ROUNDSCORE') {
+  //   return {
+  //     ...state,
+  //     players: [
+  //       ...state.players.slice(0, action.payload.index),
+  //       {
+  //         ...state.players[action.payload.index],
+  //         roundScore:
+  //           state.players[action.payload.index].roundScore +
+  //           action.payload.value,
+  //       },
+  //       ...state.players.slice(action.payload.index + 1),
+  //     ],
+  //   }
+  // }
   return state
 }
